aa8e79867750c6dd8f9fd35cf77709e26b7c83c3
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-04-17 11:44:21 +0200

    Added statistics

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1300..1400}
+for i in {1300..1450}
 do
 wget "http://www.gutenberg.org/files/$i/$i.txt"
 wget "http://www.gutenberg.org/files/$i/$i-0.txt"
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-mkdir books1
-mkdir books2
-mkdir books3
-mkdir books4
+mkdir books1 # 100MB
+mkdir books2 # 1GB
+mkdir books3 # 5GB
+mkdir books4 # 10GB
+
 chmod +x download.sh
 chmod +x mapper.sh
 chmod +x reducer.sh
@@ -13,25 +14,33 @@ cd ..
 
 for i in {1..10}
 do
-cp --backup=numbered ./books1/* ./books2
+    cp --backup=numbered ./books1/* ./books2
 done
 
-for i in {1..20}
+for i in {1..5}
 do
-cp --backup=numbered ./books1/* ./books3
+    cp --backup=numbered ./books2/* ./books3
 done
 
-for i in {1..30}
+for i in {1..2}
 do
-cp --backup=numbered ./books1/* ./books4
+    cp --backup=numbered ./books3/* ./books4
 done
 
 for i in {1..4}
 do
-cd books$i
-hdfs dfs -mkdir books-input$i
-hdfs dfs -put *.txt* books-input$i
-time python3 ../sequential.py >> ../result$i.txt
-cd ..
-time hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py -input books-input$i -output books-output$i
+    cd books$i
+    hdfs dfs -mkdir books-input$i
+    hdfs dfs -put *.txt* books-input$i
+        for iteration in {1..3}
+        do
+            (time python3 ../sequential.py >> ../result$i.txt) &> ../measurements.txt
+            rm ../result$i.txt
+        done
+    cd ..
+    for iteration in {1..3}
+        do
+            (time hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -files mapper.py,reducer.py -mapper mapper.py -reducer reducer.py -input books-input$i -output books-output$i) &> ../measurements.txt
+            rm -r books-output$i
+        done
 done
