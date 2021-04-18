3dd76186eecabb7dfc97f20923a94be968cadd74
kaspersapala <kasper.sapala@gmail.com>
2021-04-19 00:14:40 +0200

    fix selecting core instance types

--- run_nigga.sh
+++ run_nigga.sh
@@ -10,9 +10,10 @@ rm -rf mpr_aws
 git clone https://github.com/zarzecze/mpr_aws
 
 cluster_id=`aws emr list-clusters --active | jq -r '.Clusters[0].Id'`
-instance_type=`aws emr describe-cluster --cluster-id ${cluster_id} | jq -r ".Cluster.InstanceGroups[0].InstanceType"`
-num_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq -r ".Cluster.InstanceGroups[0].RunningInstanceCount"` + 1))
-instance_id=`aws emr describe-cluster --cluster-id ${cluster_id} | jq -r ".Cluster.InstanceGroups[0].Id"`
+
+instance_type=`aws emr describe-cluster --cluster-id ${cluster_id} | jq '.Cluster.InstanceGroups | .[] | select(.InstanceGroupType=="CORE") | .InstanceType'`
+num_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq '.Cluster.InstanceGroups | .[] | select(.InstanceGroupType=="CORE") | .RunningInstanceCount'` + 1))
+instance_id=`aws emr describe-cluster --cluster-id ${cluster_id} | jq '.Cluster.InstanceGroups | .[] | select(.InstanceGroupType=="CORE") | .Id'`
 
 bucket_name=`echo ${cluster_id} | awk '{print tolower($0)}'`
 
@@ -27,16 +28,15 @@ for i_sizes in "${instances_to_request[@]}"; do
 
         aws emr modify-instance-groups --cluster-id ${cluster_id} --instance-groups '[{"InstanceGroupId": "'${instance_id}'", "InstanceCount": '${i_sizes}'}]'
 
-        requested_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq -r ".Cluster.InstanceGroups[0].RequestedInstanceCount"` + 1))
-
+        requested_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq '.Cluster.InstanceGroups | .[] | select(.InstanceGroupType=="CORE") | .RequestedInstanceCount'` + 1))
 
         echo "Resizing instances from: '${num_instances}' to '${requested_instances}'"
 
         while [ $num_instances -ne $requested_instances ]
         do
                 sleep 1
-                num_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq -r ".Cluster.InstanceGroups[0].RunningInstanceCount"` + 1))
-                requested_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq -r ".Cluster.InstanceGroups[0].RequestedInstanceCount"` + 1))
+                num_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq '.Cluster.InstanceGroups | .[] | select(.InstanceGroupType=="CORE") | .RunningInstanceCount'` + 1))
+                requested_instances=$((`aws emr describe-cluster --cluster-id ${cluster_id} | jq '.Cluster.InstanceGroups | .[] | select(.InstanceGroupType=="CORE") | .RequestedInstanceCount'` + 1))
         done
 
 
